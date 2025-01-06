455627496a17b6ccbc9adf176ffbf1cb398fbbe2
Kiryu <kiryu.balbas@gmail.com>
2025-01-05 20:00:28 -0400

    dependencias de tailwind y prettiercode instaladas y añadido icono de carrito de compras (hasta ahora unicamente cambios esteticos)

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,5 +1,6 @@
 import React from "react";
-import { Search} from 'lucide-react';
+import { Search } from "lucide-react";
+import { ShoppingCart } from "lucide-react";
 
 const MenuLinks = [
   {
@@ -21,23 +22,15 @@ const MenuLinks = [
 
 const Navbar = () => {
   return (
-    <div className="container flex justify-between items-center">
+    <div className="container flex items-center justify-between">
       {/*Logo and links */}
 
-      <div className="bg-white 
-                      dark:bg-gray-900 
-                      dark:text-white 
-                      duration-200 
-                      relative z-40">
+      <div className="relative z-40 bg-white duration-200 dark:bg-gray-900 dark:text-white">
         <div className="py-4">
           <div className="flex items-center gap-4">
             <a
               href="#"
-              className="text-primary 
-                        font-semibold 
-                        tracking-wides
-                        text-2xl uppercase 
-                        sm:text-3xl"
+              className="tracking-wides text-2xl font-semibold uppercase text-primary sm:text-3xl"
             >
               Farma-nada
             </a>
@@ -47,13 +40,12 @@ const Navbar = () => {
               <ul className="flex items-center gap-6">
                 {MenuLinks.map((data, index) => (
                   <li key={index}>
-                    <a href={data.link} 
-                    className="inline-block px-4
-                              font-semibold 
-                              hover:text-gray-600 
-                              dark:text-white 
-                              duration-200 
-                              dark:hover:text-gray-500">{data.name}</a>
+                    <a
+                      href={data.link}
+                      className="inline-block px-4 font-semibold duration-200 hover:text-gray-600 dark:text-white dark:hover:text-gray-500"
+                    >
+                      {data.name}
+                    </a>
                   </li>
                 ))}
               </ul>
@@ -62,24 +54,25 @@ const Navbar = () => {
         </div>
       </div>
       {/*Navbar right section / Seccion derecha del navbar */}
-      <div className="flex justify-between items-center gap-4">
-      {/* Seccion de busqueda/Searh bar section */ }
-      <div className="relative group hidden sm:block">
-                <input type="text" placeholder="Buscar..."
-                  className="
-                  search-bar
-                  "/>
-                  <Search 
-                  className="text-xl text-gray600
-                  dark:text-gray-400
-                  "
-                  />
-                
-      {/* Modo oscuro/Dark mode section */ }
-      <div>
-
-          </div>
+      <div className="flex items-center justify-between gap-4">
+        {/* Seccion de busqueda/Searh bar section */}
+        <div className="group relative hidden sm:block">
+          <input
+            type="text"
+            placeholder="Buscar..."
+            className="w-0 rounded-full px-3 py-1 transition-all duration-300 focus:outline-none group-hover:w-[300px] group-hover:border group-hover:border-gray-500 dark:border-gray-800 dark:bg-gray-900 dark:text-gray-400 group-hover:dark:bg-gray-800"
+          />
+          <Search className="absolute right-3 top-1/2 -translate-y-1/2 text-xl text-gray-600 duration-200 group-hover:text-primary dark:text-gray-400" />
         </div>
+        {/*Order Button Section */}
+        <button className="relative p-3">
+          <ShoppingCart className="text-xl text-gray-600 hover:text-gray-400 dark:text-gray-300" />
+          <div className="iems-center absolute right-0 top-0 flex h-4 w-4 justify-center rounded-full bg-red-500 text-xs text-white">
+            4
+          </div>
+        </button>
+        {/* Modo oscuro/Dark mode section */}
+        <div></div>
       </div>
     </div>
   );
